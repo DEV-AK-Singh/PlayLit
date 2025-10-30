c5a1b1be2cbbd48d78252df60cad701ad62e0601
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-10-30 19:47:40 +0530

    fixed instance count

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -3,11 +3,10 @@ module.exports = {
     name: 'playlit-server',
     script: './dist/index.js',
     instances: 1,
-    exec_mode: 'cluster',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 5000
-    },
+    exec_mode: "fork_mode",
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
     error_file: './logs/err.log',
     out_file: './logs/out.log',
     log_file: './logs/combined.log',
